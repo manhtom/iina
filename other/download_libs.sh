43f072ab2e89ca328c269021234dc7947e9c09a2
李通洲 <carter.li@eoitek.com>
2022-06-08 09:56:28 +0800

    Revert "Update download_libs.sh to fetch the latest yt-dlp"
    This reverts commit b76d20fe304f9edb57453bc695842ddc2cf6525b.

--- other/download_libs.sh
+++ other/download_libs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 location="https://iina.io/dylibs/universal"
 IFS=$'\n' read -r -d '' -a files < <(curl "${location}/filelist.txt" && printf '\0')
 mkdir -p deps/lib
@@ -9,8 +8,3 @@ do
   curl "${location}/${file}" > deps/lib/$file
   { set +x; } 2>/dev/null
 done
-
-# Use https://iina.io/dylibs/youtube-dl to get the binary included in the latest release.
-mkdir -p deps/executable
-curl -L 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos' -o deps/executable/youtube-dl
-chmod +x deps/executable/youtube-dl
